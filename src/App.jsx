497d1f52f7bf9a12177889643a5edd3e8e72c3fc
stijn1290 <153715940+stijn1290@users.noreply.github.com>
2025-05-21 09:09:23 +0200

    Fixed layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,43 @@
-import './App.css'
-import {useEffect, useState} from "react";
+import './App.css';
+import { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
-import {db} from './config/firebase.js';
+import { db } from './config/firebase.js';
 
 function App() {
     const [getPostList, setPostList] = useState([]);
+
     useEffect(() => {
         const getPosts = async () => {
             const data = await getDocs(collection(db, "posts"));
-            setPostList(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
-        }
+            setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        };
         getPosts();
     }, []);
+
     return (
         <>
-            <h2>Posts</h2>
-            {getPostList.map(post =>{
-                return(
-                    <>
-                        <div>
-                            <h2>{post.festival}  {post.user}</h2>
+            <header>
+                <a href="/">
+                    <img src="/logo.png" alt="Festiland Logo" className="imgSize" />
+                </a>
+                <h2>Posts</h2>
+            </header>
+
+            <main className="layout">
+                {getPostList.map(post => (
+                    <div className="block" key={post.id}>
+                        <div className="firstRow">
+                            <h2>{post.Festival}</h2>
+                            <h2>{post.User}</h2>
                         </div>
                         <h2><span>Title: </span>{post.title}</h2>
                         <h2><span>Description: </span>{post.description}</h2>
-                    </>
-                )
-            })}
+                    </div>
+                ))}
+            </main>
         </>
-    )
+    );
 }
 
-export default App
+export default App;
+
